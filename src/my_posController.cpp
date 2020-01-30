55e9583da1de06778d15f194fc585a5ca8714386
Donghyun <dolphin1259@naver.com>
2020-01-30 17:07:02 +0900

    add new function class::getTarget(nh) get data from ros parameter server

--- src/my_posController.cpp
+++ src/my_posController.cpp
@@ -1,4 +1,4 @@
-#include "manipulator_study/my_posController.h"
+#include "manipulator_study/my_posController.hpp"
 
 //contructor
 posController::posController(ros::NodeHandle nh_,double hz_):rate_(hz_)
@@ -31,28 +31,27 @@ posController::posController(ros::NodeHandle nh_,double hz_):rate_(hz_)
 
   //sensor_msgs::JointState
   joint_cmd_.name.resize(TOTAL_DOF);
-  joint_cmd_.position.resize(TOTALDOF);
+  joint_cmd_.position.resize(TOTAL_DOF);
+
   for(size_t i=0; i<TOTAL_DOF; i++)
   {
     joint_cmd_.name[i] = JOINT_NAME[i];
   }
+  //-------------- Publisher and Subscriber Variables --------------
 
+  //Publisher --> NodeHandle_object.advertise<datatype>("node name",queue size)
+  joint_set_pub_ = nh_.advertise<sensor_msgs::JointState>("/Franka/joint_set",1);
+  sim_start_pub_ = nh_.advertise<std_msgs::Bool>("/startSimulation",5);
+  sim_stop_pub_ = nh_.advertise<std_msgs::Bool>("/stopSimulation",5);
+  sim_enable_sync_mode_pub_ = nh_.advertise<std_msgs::Bool>("/enableSyncMode",5);
+  trigger_next_step_pub_ = nh_.advertise<std_msgs::Bool>("/triggerNextStep",100);
+
+  //Subscriber --> NodeHandle_object.subscribe("node name",queue size,callback_function)
+  joint_state_sub_ = nh_.subscribe("/Franka/joint_states",100, &posController::jointStateCallback,this);
+  sim_step_done_sub_ = nh_.subscribe("/simulationStepDone",100, &posController::simStepDoneCallback,this);
+  sim_state_sub_ = nh_.subscribe("/simulationState",100, &posController::simStateCallback,this);
+  sim_time_sub_ = nh_.subscribe("/simulationTime",100, &posController::simTimeCallback,this);
 }
-
-//-------------- Publisher and Subscriber Variables --------------
-
-//Publisher --> NodeHandle_object.advertise<datatype>("node name",queue size)
-joint_set_pub_ = nh_.advertise<sensor_msgs::JointState>("/Franaka/joint_set",1);
-sim_start_pub_ = nh_.advertise<std_msgs::Bool>("/startSimulation",5)
-sim_stop_pub_ = nh_.advertise<std_msgs::Bool>("/stopSimulation",5)
-sim_enable_sync_mode_pub_ = nh_.advertise<std_msgs::Bool>("/enableSyncMode",5)
-trigger_next_step_pub_ = nh_.advertise<std_msgs::Bool>("/triggerNextStep",100);
-
-//Subscriber --> NodeHandle_object.subscribe("node name",queue size,callback_function)
-joint_state_sub_ = nh_.subscribe("/Franaka/joint_states",100, &posController::jointStateCallback);
-sim_step_done_sub_ = nh_.subscribe("/simulationStepDone",100, &posController::simStepDoneCallback);
-sim_state_sub_ = nh_.subscribe("/simulationState",100, &posController::simStateCallback);
-sim_time_sub_ = nh_.subscribe("/simulationTime",100, &posController::simTimeCallback);
 
 //destructor
 posController::~posController()
@@ -63,7 +62,8 @@ posController::~posController()
 //-------------- Callback Functions for subscribing topics --------------
 
 //joint Callback function
-void posController::jointStateCallback(sensor_msgs::JointStateConstPtr &msg)
+
+void posController::jointStateCallback(const sensor_msgs::JointStateConstPtr& msg)
 {
   if(msg->name.size() == TOTAL_DOF)
   {
@@ -71,7 +71,6 @@ void posController::jointStateCallback(sensor_msgs::JointStateConstPtr &msg)
     {
       q_[i] = msg->position[i];
       q_dot_[i] = msg->velocity[i];
-
     }
   }
   else
@@ -79,19 +78,20 @@ void posController::jointStateCallback(sensor_msgs::JointStateConstPtr &msg)
     ROS_ERROR("Controller's total Dof and JointStates from VREP is not same size!!");
   }
 }
+
 //simulation state callback 0: stopped 1:running 2: paused
-void posController::simStateCallback(std_msgs::Int32ConstPtr &msg)
+void posController::simStateCallback(const std_msgs::Int32ConstPtr& msg)
 {
   vrep_sim_status_ = msg->data;
 }
 //simulation time callback
-void posController::simTimeCallback(std_msgs::Float32ConstPtr &msg)
+void posController::simTimeCallback(const std_msgs::Float32ConstPtr& msg)
 {
   sim_time_ = msg->data;
-  tick = (sim_time_)/SIM_DT; // sec per period
+  tick_ = (sim_time_)/SIM_DT; // sec per period
 }
 //simulation step done callback
-void posController::simStepDoneCallback(std_msgs::BoolConstPtr &msg)
+void posController::simStepDoneCallback(const std_msgs::BoolConstPtr& msg)
 {
   sim_step_done_ = msg->data;
 }
@@ -148,7 +148,7 @@ void posController::setDesiredPos(std::vector<float> des_q)
   {
     for(size_t i=0;i<TOTAL_DOF;i++)
     {
-      desired_q_(i) = deg2rad(dq.at(i));
+      desired_q_(i) = deg2rad(des_q.at(i));
     }
   }
   else
@@ -159,7 +159,7 @@ void posController::setDesiredPos(std::vector<float> des_q)
     }
     desired_q_(5) = deg2rad(45);
     desired_q_(6) = deg2rad(45);
-    desired_q_(7) = deg2rad(45)
+    desired_q_(7) = deg2rad(45);
   }
 }
 void posController::setExecTime(float t)
@@ -174,10 +174,10 @@ void posController::compute()
     final_time_ = start_time_ + exec_time_;
     is_first_run_ = false;
   }
-  current_time_ = tick*SIM_DT;
+  current_time_ = tick_*SIM_DT;
   for(size_t i=0;i<TOTAL_DOF;i++)
   {
-    target_q_(i)=cubic(current_time_, start_time_, final_time_, init_q_(i), desired_q_(i),0.0,0.0);
+    target_q_(i)=cubicSpline(current_time_, start_time_, final_time_, init_q_(i), desired_q_(i),0.0,0.0);
   }
 }
 void posController::wait()
@@ -188,4 +188,27 @@ void posController::wait()
   }
   sim_step_done_ = false;
   rate_.sleep();
+}
+
+void posController::getTarget(ros::NodeHandle nh_)
+{
+
+  if (final_time_ < current_time_)
+  {
+    float joint_value[TOTAL_DOF];
+    for(size_t i=0;i<TOTAL_DOF;i++)
+    {
+      nh_.getParam("j"+ToString(i+1),joint_value[i]);
+      //ROS_INFO("%0.6f",joint_value[i]);
+    }
+    nh_.getParam("exec_time",exec_time_);
+
+    for(size_t i=0;i<TOTAL_DOF;i++)
+    {
+      init_q_(i)= desired_q_(i);
+      desired_q_(i) = deg2rad(joint_value[i]);
+    }
+    is_first_run_ = true;
+
+  }
 }
